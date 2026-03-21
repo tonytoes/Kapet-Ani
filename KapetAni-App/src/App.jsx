9e7475b145a5294ba05c0b1287a57933d6599f64
juntaror <qynmiura@tip.edu.ph>
2026-03-21 20:53:05 +0800

    Add Blogs page, styles and author images

--- KapetAni-App/src/App.jsx
+++ KapetAni-App/src/App.jsx
@@ -2,12 +2,13 @@ import { useEffect } from "react";
 import Home from "./pages/Home.jsx";
 import Order from "./pages/Order.jsx";
 import Footer from "./components/layout/Footer.jsx";
+import Blogs from "./pages/Blogs.jsx";
 
 function App() {
   return (
  <>
       <div className="App">
-        <Home/>
+        <Blogs/>
       </div>
     </>
   );
