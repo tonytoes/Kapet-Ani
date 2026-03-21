5d54fd938870575640bafd3bd6cd4bb74497f1eb
tonytoes <159160091+tonytoes@users.noreply.github.com>
2026-03-21 21:02:52 +0800

    feat: Login and Review draft

--- KapetAni-App/src/App.jsx
+++ KapetAni-App/src/App.jsx
@@ -1,14 +1,14 @@
 import { useEffect } from "react";
 import Home from "./pages/Home.jsx";
 import Order from "./pages/Order.jsx";
+import Login from "./pages/Login.jsx";
+import Review from "./pages/Review.jsx";
 import Footer from "./components/layout/Footer.jsx";
 
 function App() {
   return (
- <>
-      <div className="App">
-        <Home/>
-      </div>
+    <>
+      <Review/>
     </>
   );
 }
