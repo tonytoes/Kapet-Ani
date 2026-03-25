677f0171d11b8dc20da44b34295242fed23b5976
tonytoes <159160091+tonytoes@users.noreply.github.com>
2026-03-25 19:26:05 +0800

    feat: added user icon

--- KapetAni-App/src/components/layout/Navbar.jsx
+++ KapetAni-App/src/components/layout/Navbar.jsx
@@ -66,6 +66,12 @@ function Navbar({ activePage }) {
             </li>
           </ul>
           <div className="nav-cart">
+            <a href="/login" className="nav-icon">
+              <svg width={16} height={16} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8">
+                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
+                <circle cx="12" cy="7" r="4" />
+              </svg>
+            </a>
             <svg
               width={16}
               height={16}
